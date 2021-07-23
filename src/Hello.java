b100b3d76eec750e06ef1fa1f1d07900b2f301c0
pengjiahan <1250308041@qq.com>
2021-07-23 17:08:11 +0800

    friday over

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 /**
  * @author PJH
  */
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("HelloWorld!");
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        System.out.printf("%5d",num);
     }
 }
+
